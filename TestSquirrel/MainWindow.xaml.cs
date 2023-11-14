32efe5ae8ba89a1096310a338ced06387e7bca99
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-11-14 10:36:24 +0200

    First commit

---
+++ TestSquirrel/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Windows;
+using Squirrel;
+
+namespace TestSquirrel;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        var assembly = Assembly.GetExecutingAssembly();
+        var fileInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+        VersionTb.Text = fileInfo.FileVersion;
+    }
+
+    private void Check_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Update_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private async void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        // var manager = await UpdateManager.GitHubUpdateManager();
+    }
+}
