72e845b70644d71f152499a89d39f8b27a20ff58
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-11-14 17:52:45 +0200

    Change button Color

--- TestSquirrel/AssemblyInfo.cs
+++ TestSquirrel/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None,ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3")]
